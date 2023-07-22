b5ca4b2aa447fa9d61a899704627022b587cabf9
raneelams <vfxnaveen827@gmail.com>
2023-07-23 00:53:46 +0530

    Adding Coaches to Vuex

--- src/store/moduels/coaches/actions.js
+++ src/store/moduels/coaches/actions.js
@@ -1,1 +1,14 @@
-export default {}
+export default {
+    registerCoach(context, data) {
+      const coachData = {
+        id: context.rootGetters.userId, //getting user id from index.js in store
+        firstName: data.first,
+        lastName: data.last,
+        description: data.desc,
+        hourlyRate: data.rate,
+        areas: data.areas
+      };
+  
+      context.commit('registerCoach', coachData); //commiting this mutation with the transfrom coach data.
+    }
+  };
--- src/store/moduels/coaches/getters.js
+++ src/store/moduels/coaches/getters.js
@@ -4,5 +4,10 @@ export default {
     },
     hasCoaches(state) {
         return state.coaches && state.coaches.length > 0;
-    }
+    },
+    isCoach(_, getters, _2, rootGetters) { //_, _2 denotes like the arguments are taking in methods but not using as of now.
+        const coaches = getters.coaches;
+        const userId = rootGetters.userId;
+        return coaches.some(coach => coach.id === userId); //some is a bulitin method which returns true if somecoach fulfills criteria
+      }
 };
