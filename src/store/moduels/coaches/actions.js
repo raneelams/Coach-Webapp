dc11de71e306aeb810ed3694e8c6ee75da94e326
raneelams <vfxnaveen827@gmail.com>
2023-07-23 12:28:02 +0530

    Updated with google firebase database

--- src/store/moduels/coaches/actions.js
+++ src/store/moduels/coaches/actions.js
@@ -1,7 +1,7 @@
-export default {
-    registerCoach(context, data) {
+  export default {
+    async registerCoach(context, data) {
+      const userId = context.rootGetters.userId; //getting user id from index.js in store
       const coachData = {
-        id: context.rootGetters.userId, //getting user id from index.js in store
         firstName: data.first,
         lastName: data.last,
         description: data.desc,
@@ -9,6 +9,20 @@ export default {
         areas: data.areas
       };
   
-      context.commit('registerCoach', coachData); //commiting this mutation with the transfrom coach data.
+      const response = await fetch(`https://vue-http-demo-a064c-default-rtdb.firebaseio.com//coaches/${userId}.json`, {
+        method: 'PUT',
+        body: JSON.stringify(coachData)
+      });
+  
+      // const responseData = await response.json();
+  
+      if (!response.ok) {  //okay is the built in property to get th boolean value of response.
+        // error ...
+      }
+  
+      context.commit('registerCoach', {
+        ...coachData,
+        id: userId
+      }); //commiting this mutation with the transfrom coach data.
     }
   };
